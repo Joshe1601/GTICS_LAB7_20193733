f432d3130110f88bf1e6fe3f695007be3a3ccc52
Jose Morillos <jmorillosp@pucp.edu.pe>
2023-09-28 20:38:36 -0500

    Empezamos el lab

---
+++ src/main/java/com/example/lab7_gtics_20193733/Lab7Gtics20193733Application.java
@@ -0,0 +1,13 @@
+package com.example.lab7_gtics_20193733;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab7Gtics20193733Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab7Gtics20193733Application.class, args);
+    }
+
+}
